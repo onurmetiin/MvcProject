8cb6a661678b4f5bdd0fdc7de84d6f5d718147c7
Onur METİN <onurmetin95@gmail.com>
2021-04-24 19:06:14 +0300

    Add project files.

---
+++ DataAccessLayer/Concrete/Repositories/GenericRepository.cs
@@ -0,0 +1,50 @@
+﻿using DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+
+        Context con = new Context();
+        DbSet<T> _object;
+
+        public GenericRepository()
+        {
+            _object = con.Set<T>();
+        }
+
+        public void Add(T t)
+        {
+            _object.Add(t);
+            con.SaveChanges();
+        }
+
+        public void Delete(T t)
+        {
+            _object.Remove(t);
+            con.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            return _object.ToList();
+        }
+
+        public List<T> List(Expression<Func<T, bool>> filter)
+        {
+            return _object.Where(filter).ToList();
+        }
+
+        public void Update(T t)
+        {
+            con.SaveChanges();
+        }
+    }
+}
---
+++ EntityLayer/Concrete/Header.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Header
+    {
+        [Key]
+        public int HeaderID { get; set; }
+        [StringLength(50)]
+        public string HeaderName { get; set; }
+        public DateTime HeaderDate { get; set; }
+
+
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+        public int WriterID { get; set; }
+        public virtual Writer Writer { get; set; }
+
+        public ICollection<Content> Contents { get; set; }
+    }
+}
